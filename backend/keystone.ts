bfaaee5f7c15a31c7b1acd0563a7ea4044210650
Annie Liao <liaowow@gmail.com>
2021-03-10 16:07:57 -0500

    insert seed data into database

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -5,6 +5,7 @@ import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
 import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
+import { insertSeedData } from './seed-data';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-fightcovidgear';
 
@@ -33,7 +34,11 @@ export default withAuth(config({
   db: {
     adapter: 'mongoose',
     url: databaseURL,
-    // add data seeding
+    async onConnect(keystone) {
+      if (process.argv.includes('--seed-data')) {
+        await insertSeedData(keystone);
+      }
+    }
   },
   lists: createSchema({
     // schema items here
