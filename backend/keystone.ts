3ffbc56c9da82d9ecbbda4095f34e2a32f752519
Annie Liao <liaowow@gmail.com>
2021-03-10 12:26:10 -0500

    set up keystone dev server

---
+++ backend/keystone.ts
@@ -0,0 +1,31 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-fightcovidgear';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // define how long the user can stay signed in
+  secret: process.env.COOKIE_SECRET,
+}
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    }
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // add data seeding
+  },
+  lists: createSchema({
+    // schema items here
+  }),
+  ui: {
+    // change for roles
+    isAccessAllowed: () => true,
+  },
+  // add session values here
+});
