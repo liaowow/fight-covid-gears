f8ca56e0ee3ac36e322f6d53b1071854724bb714
Annie Liao <liaowow@gmail.com>
2021-03-10 15:06:55 -0500

    set up product and productImage data types and hook up cloudinary

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,7 +1,9 @@
 import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
-import { User } from './schemas/User';
 import 'dotenv/config';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
 import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-fightcovidgear';
@@ -36,6 +38,8 @@ export default withAuth(config({
   lists: createSchema({
     // schema items here
     User,
+    Product,
+    ProductImage
   }),
   ui: {
     // Show this UI only for people who pass this test
