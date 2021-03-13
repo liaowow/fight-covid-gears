92b928afeafe54130b4cb23640d294be3b1b9ef6
Annie Liao <liaowow@gmail.com>
2021-03-13 12:38:40 -0500

    set up custom hooks to handle form inputs dynamically

---
+++ frontend/components/CreateProduct.js
@@ -0,0 +1,41 @@
+import useForm from '../lib/useForm';
+
+export default function CreateProduct() {
+  const { inputs, handleChange, resetForm, clearForm } = useForm({
+    name: 'Annie',
+    price: 2354,
+  });
+
+  return (
+    <form>
+      <label htmlFor="name">
+        Name
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          value={inputs.name}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="price">
+        Price
+        <input
+          type="number"
+          id="price"
+          name="price"
+          placeholder="price"
+          value={inputs.price}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="button" onClick={clearForm}>
+        Clear Form
+      </button>
+      <button type="button" onClick={resetForm}>
+        Reset Form
+      </button>
+    </form>
+  );
+}
