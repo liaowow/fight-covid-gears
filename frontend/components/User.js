6b39767f90a928228b525c03d5c926d45bacc494
Annie Liao <liaowow@gmail.com>
2021-03-15 17:41:55 -0400

    add sign-out component and functionality

--- frontend/components/User.js
+++ frontend/components/User.js
@@ -1,7 +1,7 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 
-export const CURRENT_USER_QUERY = gql`
+const CURRENT_USER_QUERY = gql`
   query {
     authenticatedItem {
       ... on User {
@@ -17,3 +17,5 @@ export function useUser() {
   const { data } = useQuery(CURRENT_USER_QUERY);
   return data?.authenticatedItem;
 }
+
+export { CURRENT_USER_QUERY };
