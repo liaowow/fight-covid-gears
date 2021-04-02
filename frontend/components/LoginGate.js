adc79f87498b150d3e807cdd3418fed53ac3f54a
Annie Liao <liaowow@gmail.com>
2021-04-02 16:02:08 -0400

    minor syntax fix on LoginGate

--- frontend/components/LoginGate.js
+++ frontend/components/LoginGate.js
@@ -1,7 +1,7 @@
 import { useUser } from './User';
 import Login from './Login';
 
-export default function({ children }) {
+export default function ({ children }) {
   const me = useUser();
   if (!me) return <Login />;
   return children;
